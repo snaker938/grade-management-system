643203e4eb6f2d64ab070da0716b37386ebf064e
Joshua <jmendel123@gmail.com>
2024-12-08 21:05:13 +0000

    Added registration repo

---
+++ backend/src/main/java/uk/ac/ucl/comp0010/repository/RegistrationRepository.java
@@ -0,0 +1,15 @@
+package uk.ac.ucl.comp0010.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import uk.ac.ucl.comp0010.model.Registration;
+
+/**
+ * Repository interface for managing Registration entities.
+ * 
+ * Registration data can be accessed at /registrations
+ */
+@RepositoryRestResource(path = "registrations", collectionResourceRel = "registrations", itemResourceRel = "registration")
+public interface RegistrationRepository extends CrudRepository<Registration, Long> {
+  // Additional custom queries can be defined here if needed.
+}
