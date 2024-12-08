d3267f44aa03ffde9efbc2705c16b3f77bd4eaaa
Joshua <jmendel123@gmail.com>
2024-12-08 21:00:47 +0000

    Created StudentRepo

---
+++ backend/src/main/java/uk/ac/ucl/comp0010/repository/StudentRepository.java
@@ -0,0 +1,15 @@
+package uk.ac.ucl.comp0010.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import uk.ac.ucl.comp0010.model.Student;
+
+/**
+ * Repository interface for managing Student entities.
+ * 
+ * Student data can be accessed at /students
+ */
+@RepositoryRestResource(path = "students", collectionResourceRel = "students", itemResourceRel = "student")
+public interface StudentRepository extends CrudRepository<Student, Long> {
+  // Additional custom queries can be defined here if needed.
+}
